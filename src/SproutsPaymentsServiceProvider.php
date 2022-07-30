156f6e50a49988f99e1577460e10d7da2c9b1c3d
Jerson <jerson@innovativesprout.com>
2022-07-30 17:02:52 +0800

    Initial

--- src/SproutsPaymentsServiceProvider.php
+++ src/SproutsPaymentsServiceProvider.php
@@ -16,7 +16,7 @@ class SproutsPaymentsServiceProvider extends ServiceProvider{
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '../config/sproutspayments.php' => config_path('sproutspayments.php')
+            __DIR__ . '/../config/sproutspayments.php' => config_path('sproutspayments.php')
         ]);
     }
 
